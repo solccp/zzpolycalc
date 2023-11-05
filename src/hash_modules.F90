solccp/zzpolycalc

commit abeadb12e6b508729013e4a76bf96c40dd39cfce

Options for reading and writing cache, slecting databse and bucket sized. Writing frequencies of cached structures.

src/hash_modules.F90
@@ -38,6 +38,7 @@ use, intrinsic :: iso_c_binding
   logical :: firstrun = .true.
   integer(int64) :: nstruct = 0
   integer(int64) :: nstructall = 0
+  integer(int64) :: nstructseen = 0
   type,public :: neigh
      integer(C_signed_char), pointer :: nlist(:)     
      integer(int16) :: order
@@ -62,6 +63,7 @@ contains
 subroutine add_neigh(nat,nbnum,a,order,poly,hashseen,iseen,lastseen,duringread)
 !  USE IFPORT ! for rename
   use hash_module
+  use options_module
   implicit none
   integer(kint),intent(in) :: nat
   integer(kint), intent(in) :: nbnum(nat)
@@ -99,7 +101,7 @@ subroutine add_neigh(nat,nbnum,a,order,poly,hashseen,iseen,lastseen,duringread)
 !    stop
 !  end if
 
-  if (.not. present(duringread)) then 
+  if (.not. present(duringread)) then ! only print in main calculation not when reading cache.bin
     nstructall=nstructall+1
     if (mod(nstructall,1000000).eq.0)  then
       call system_mem_usage(mem)
@@ -272,13 +274,15 @@ end if
 
   if (.not.present(hashseen) .and. .not. present(duringread)) then
    if (mod(nstructall,writemark).eq.0)  then
-    write(*,*)'Saving cache'
-    call execute_command_line ("mv cache.bin cache.bin.bak")
+    if (has_write_cache_file) then   
+      write(*,*)'Saving cache',write_cache_fname
+!    call execute_command_line ("mv cache.bin cache.bin.bak")
 !     ires=rename('cache.bin','cache.bin.bak')
-     call writetodisk
+      call writetodisk(write_cache_fname)
 !    call execute_command_line ("rm cache.bin.bak")
-     open(unit=23, iostat=ires, file='cache.bin.bak', status='old')
-     if (ires .eq. 0) close(23, status='delete')
+!     open(unit=23, iostat=ires, file='cache.bin.bak', status='old')
+!     if (ires .eq. 0) close(23, status='delete')
+    end if 
    end if
   end if
 
@@ -302,7 +306,7 @@ end if
 end subroutine add_neigh
 
 
-subroutine writetodisk
+subroutine writetodisk(fname)
 use hash_module
 implicit none
   type(ptrneigh),pointer :: curr
@@ -313,9 +317,13 @@ implicit none
   integer :: i,j,ilen,idx2,idx1,ihighscore
   integer(C_signed_char) :: hashsum(hashsize)
   integer(kint) :: leadpowmax=0
+  character(len=*), intent(in) :: fname
 
-  open(23,file='cache.bin',FORM='UNFORMATTED')
+  open(23,file=fname,FORM='UNFORMATTED')
   write(23)vlongmax,nstructall
+  open(24,file='cache.txt',FORM='FORMATTED')
+  write(23)vlongmax,nstructall
+
 
   do idx1=1,nbuckets
     xtmp=x(idx1)
@@ -329,6 +337,7 @@ implicit none
     end if
     do i=1,ilen
       write(23)x(idx1)%p(i)%order,x(idx1)%p(i)%iseen,x(idx1)%p(i)%nat,x(idx1)%p(i)%lastseen,x(idx1)%p(i)%nlist
+      write(24,*)x(idx1)%p(i)%nat,x(idx1)%p(i)%iseen,x(idx1)%p(i)%order
       call writetofile(23,x(idx1)%p(i)%polynomial,x(idx1)%p(i)%order+1)
       do j=1,x(idx1)%p(i)%order+1
         if (x(idx1)%p(i)%polynomial(j)%leadpow.gt.leadpowmax) leadpowmax=x(idx1)%p(i)%polynomial(j)%leadpow
@@ -338,10 +347,11 @@ implicit none
   end do
   write(*,*)'Max large integer size: ',leadpowmax
   close(23)
+  close(24)
 end subroutine writetodisk
 
 
-subroutine readfromdisk
+subroutine readfromdisk(fname)
 use hash_module
 implicit none
   integer(C_signed_char) :: hashsum(hashsize)
@@ -355,12 +365,13 @@ implicit none
      integer(kint) :: iseen
      integer(int64) :: lastseen
      type(vlonginteger),allocatable :: poly(:)
+  character(len=*), intent(in) :: fname
 
    allocate(x(nbuckets),xlen(nbuckets),irepl(nbuckets))
    firstrun=.false.
    xlen=0
-  write(*,*)'Reading cache.bin'
-  open(23,file='cache.bin',FORM='UNFORMATTED')
+  write(*,*)'Reading cache', fname
+  open(23,file=fname,FORM='UNFORMATTED')
   read(23)vlong,nstructall
   if (vlong.gt.vlongmax) then
     write(*,*)'WARNING!'
@@ -382,7 +393,7 @@ implicit none
   end if
   end do
   close(23)  
-  write(*,*)'cache.bin processed'
+  write(*,*)'cache processed'
 
 end subroutine readfromdisk
 
@@ -408,6 +419,7 @@ function check_seen(nat,nbnum,a,order,poly) result(seen)
   type(neigh) :: temp
   integer(int64), pointer :: temp2
 
+  nstructseen = nstructseen + 1
 
   if (firstrun) then
     allocate(x(nbuckets),xlen(nbuckets),irepl(nbuckets))
@@ -417,7 +429,7 @@ function check_seen(nat,nbnum,a,order,poly) result(seen)
   end if
 
 !  if (nat.le.10 ) return
-
+  
   asmall=0 ! fills outside of neighbornumber with zeroes
   do i=1,nat
    do j=1,nbnum(i)

src/findZZ.f90
@@ -52,7 +52,6 @@ recursive subroutine find_ZZ_polynomial(pah,level,path)
 
     seen=check_seen(pah%nat,pah%neighbornumber,pah%neighborlist,pah%order,pah%polynomial)
 !    write(*,*)seen
- 
 
     if (.not. seen) then
       call decompose(pah,level,path)

src/getopt.f90
@@ -76,7 +76,7 @@ subroutine read_options(input_fname)
 
 
     do
-        okey = getopt('hap')
+        okey = getopt('ham:pr:s:w:')
         if(okey == '>') exit
         if(okey == '!') then
             write(*,*) 'unknown option: ', trim(optarg)
@@ -85,8 +85,26 @@ subroutine read_options(input_fname)
         if(okey == 'a') then
             is_adjacencyfile = .true.
         end if
+        if(okey == 'm') then
+            read(optarg, *) maxrecords
+        end if
+
         if(okey == 'p') then
             print_bondlevel = .true.
+        end if
+
+        if(okey == 'r') then
+            read(optarg, '(a)') read_cache_fname
+            has_read_cache_file = .true. 
+        end if
+
+        if(okey == 's') then
+            read(optarg, *) nbuckets
+        end if
+
+        if(okey == 'w') then
+            read(optarg, '(a)') write_cache_fname
+            has_write_cache_file = .true. 
         end if
 
 
@@ -106,7 +124,11 @@ subroutine print_usage()
     write(*, '(1x,a)') "Usage: ZZPolyCalc [options] input"
     write(*, '(1x,a)') "Options:"
     write(*, '(1x,10a)') "    ", "-a", "                ",  "Input file specifies adjacency instead of Cartesian geometry"
+    write(*, '(1x,10a)') "    ", "-m number", "         ",  "Maximum {number} of structures in cache database"
     write(*, '(1x,10a)') "    ", "-p", "                ",  "Print intermediate bondlevel structures"
+    write(*, '(1x,10a)') "    ", "-r file", "           ",  "Read cached structures from {file}"
+    write(*, '(1x,10a)') "    ", "-s number", "         ",  "Use {number} of buckets in cache database"
+    write(*, '(1x,10a)') "    ", "-w file", "           ",  "Write cached structures to {file}"
     write(*, '(1x,10a)') "    ", "-h", "                ",  "Show this message"
 end subroutine
 

src/options.f90
@@ -6,13 +6,19 @@ module options_module
   integer(int64) :: writemark 
   integer :: maxrecords 
   logical :: is_adjacencyfile
+  logical :: has_read_cache_file
+  character(len=100) :: read_cache_fname
+  logical :: has_write_cache_file
+  character(len=100) :: write_cache_fname
   logical :: print_bondlevel 
 contains
   subroutine initialize_options()
     nbuckets = 2097152
     writemark =   200000000
-    maxrecords =  12000000
+    maxrecords =  52000000
     is_adjacencyfile = .false.
     print_bondlevel = .false. 
+    has_read_cache_file = .false.
+    has_write_cache_file = .false. 
   end subroutine
 end module

src/types_module.f90
@@ -8,7 +8,7 @@ use ISO_FORTRAN_ENV
   integer, parameter :: kint = 4
   integer, parameter :: kreal = kind(0.0d0)
   integer, parameter :: maxatoms = 50000
-  integer, parameter :: vlongmax = 27
+  integer, parameter :: vlongmax = 14
   integer, parameter :: vbase = 1000000000
   integer, parameter :: maxpolylength = 1000
 

src/zhang.F90
@@ -44,9 +44,14 @@ program zhang_polynomial
   
   
 ! read cache from disk 
-  inquire(file='cache.bin',exist=cacheexists)
-  if (cacheexists) then
-    call readfromdisk
+  if (has_read_cache_file) then 
+    inquire(file=read_cache_fname,exist=cacheexists)
+    if (cacheexists) then
+      call readfromdisk(read_cache_fname)
+    else
+      write(*,*)'Cache file',read_cache_fname,'does not exist'
+      stop
+    end if
   end if
 
 ! #############################################################
@@ -58,7 +63,7 @@ program zhang_polynomial
 ! # print the ZZ polynomial #
 ! ###########################
   call print_ZZ_polynomial(pah)
-  write(*,*)'Unique',nstruct
+  write(*,*)'Seen Unique Remembered',nstructseen,nstructall,nstruct
   notused=0
   do i=1,nbuckets
     do j=1,xlen(i)
@@ -68,8 +73,9 @@ program zhang_polynomial
   end do
 
   write(*,*)'Not used:',notused
-!  call writetodisk
-
+  if (has_write_cache_file) then
+    call writetodisk(write_cache_fname)
+  end if
 end
 !####################################################################################
 !###################### end of program zhang_polynomial #############################

commit 8562cb74e9a9105a4174a3c1a98ced3a275fe84f

Fix stupid bug introduced in abeadb12 that made the cached file silently unreadable

src/hash_modules.F90
@@ -262,7 +262,7 @@ implicit none
   open(23,file=fname,FORM='UNFORMATTED')
   write(23)vlongmax,nstructall
   open(24,file='cache.txt',FORM='FORMATTED')
-  write(23)vlongmax,nstructall
+!  write(23)vlongmax,nstructall
 
 
   do idx1=1,nbuckets