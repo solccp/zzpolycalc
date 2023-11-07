1b70204d5cb59b109787f09dfba36f23e8be7c43
Rafal Podeszwa <poszwa@tiger.chem.uw.edu.pl>
2023-11-07 13:58:24 +0100

    Removed non-portanble memoryusage function. Current filesize estimate provides an estimate of memory

--- src/hash_modules.F90
+++ src/hash_modules.F90
@@ -87,14 +87,12 @@ subroutine add_neigh(nat,nbnum,a,order,poly,hashseen,iseen,lastseen,duringread)
   logical,OPTIONAL :: duringread
 
   logical :: replace
-  integer :: mem
   integer :: mpacksize
 
   if (.not. present(duringread)) then ! only print in main calculation not when reading cache.bin
     nstructall=nstructall+1
     if (mod(nstructall,100000_int64).eq.0)  then
-      call system_mem_usage(mem)
-      if (verbose) write(*,'(A,2I0,A,2I0)')'nstruct',nstructall,nstruct,' mem',mem,ncachebytes
+      if (verbose) write(*,'(A,2I0,A,2I0)')'nstruct',nstructall,nstruct,' filesize',ncachebytes
     end if 
   end if
 
@@ -388,52 +386,4 @@ function check_seen(nat,nbnum,a,order,poly) result(seen)
 
 end function check_seen
 
-
-!from stackexchange
-subroutine system_mem_usage(valueRSS)
-#ifdef __INTEL_COMPILER
-use ifport !if on intel compiler
-#endif
-
-implicit none
-integer, intent(out) :: valueRSS
-
-character(len=200):: filename=' '
-character(len=80) :: line
-character(len=8)  :: pid_char=' '
-integer :: pid
-logical :: ifxst
-
-valueRSS=-1    ! return negative number if not found
-
-!--- get process ID
-
-pid=getpid()
-write(pid_char,'(I8)') pid
-filename='/proc/'//trim(adjustl(pid_char))//'/status'
-
-!--- read system file
-
-inquire (file=filename,exist=ifxst)
-if (.not.ifxst) then
-  write (*,*) 'system file does not exist'
-  return
-endif
-
-open(unit=100, file=filename, action='read')
-do
-  read (100,'(a)',end=120) line
-  if (line(1:6).eq.'VmRSS:') then
-     read (line(7:),*) valueRSS
-     exit
-  endif
-enddo
-120 continue
-close(100)
-
-return
-end subroutine system_mem_usage
-
-
 end module lookup_module_hash
-
